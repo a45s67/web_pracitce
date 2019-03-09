c0400afa5130468386ac6534e75893690c502183
fish <b45s67@gmail.com>
2019-03-10 00:28:23 +0800

    init

---
+++ app/controllers/kamigo_controller.rb
@@ -0,0 +1,44 @@
+def translate_to_korean(message)
+    "#{message}油~"
+end
+
+class KamigoController < ApplicationController
+	def eat 
+		render plain: "屎"
+	end
+
+	def request_headers
+		render plain: request.headers.to_h.reject{|key,value| key.include? '.'}.map{|key,value| "#{key}: #{value}"}.sort.join("\n")
+	end
+
+	def request_body
+		render plain: requset.request_body
+	end
+
+	def response_headers
+		response.headers['5566'] = '不能亡'
+		render plain: response.headers.to_h.map{ |key, value|
+	      "#{key}: #{value}"
+	    }.sort.join("\n")
+	end
+	def show_response_body
+	    puts "===這是設定前的response.body:#{response.body}==="
+	    render plain: "虎哇花哈哈哈"
+	    puts "===這是設定後的response.body:#{response.body}==="
+	end
+
+	def sent_request
+		uri = URI('http://localhost:3000/kamigo/response_body')
+		#response = Net::HTTP.get(uri).force_encoding("UTF-8")
+		http = Net::HTTP.new(uri.host , uri.port)
+		http_request = Net::HTTP::Get.new(uri)
+		http_response = http.request(http_request)
+
+		render plain: JSON.pretty_generate({
+	      request_class: request.class,
+	      response_class: response.class,
+	      http_request_class: http_request.class,
+	      http_response_class: http_response.class
+	    })
+	end
+end
